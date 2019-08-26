a0e54c9f35887c9e4f958303cba4e44c14010e89
Chris Kraft <kibby111701@gmail.com>
2019-08-26 12:41:01 -0600

    Correct change code

---
+++ Chapter12/CorrectChange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class  CorrectChange{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int changeDue, dollars, quarters, dimes, nickels, pennies;
+        
+        System.out.println("Input the change due:");
+        changeDue = scan.nextInt();
+        dollars = changeDue/100;
+        changeDue = changeDue % 100;
+        quarters = changeDue/25;
+        changeDue = changeDue % 25;
+        dimes = changeDue/10;
+        changeDue = changeDue % 10;
+        nickels = changeDue/5;
+        pennies = changeDue % 5;
+
+        System.out.println("Your change is: " + dollars + " dollars, " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels and " + pennies + " pennies.");
+
+        
+    }
+
+
+
+}
