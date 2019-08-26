6c88e636990779efdc70b168db765b024e978ab8
Chris Kraft <kibby111701@gmail.com>
2019-08-26 12:54:34 -0600

    Finished Ohms Law

--- Chapter12/OhmsLaw.java
+++ Chapter12/OhmsLaw.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class OhmsLaw{
     public static void main(String[] args) {
-        
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        int voltage, resistance; 
+        double current;
+
+        System.out.println("Gimme the voltage");
+        voltage = scan1.nextInt();
+        System.out.println("Gimme the resistance");
+        resistance = scan2.nextInt();
+
+        current = (voltage + 0.0)/(resistance + 0.0);
+        System.out.println("The current is " + current + " amps.");
     }
 
 
