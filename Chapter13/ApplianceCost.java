a87c79d4b96b771f38d380fc761183bf4957d19f
Chris Kraft <kibby111701@gmail.com>
2019-08-27 12:31:10 -0600

    Finished Appliance code

--- Chapter13/ApplianceCost.java
+++ Chapter13/ApplianceCost.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class ApplianceCost{
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        double costPerHour, annualCost;
+        int kHour;
+
+
+        System.out.println("Enter cost per kilowatt-hour in cents");
+        costPerHour = scan.nextDouble();
+        System.out.println("Enter kilowatt-hours used per year");
+        kHour = scan.nextInt();
+
+        annualCost = (costPerHour * kHour) / 100;
+
+        System.out.println("Annual cost: " + annualCost);
+
     }
 
 
