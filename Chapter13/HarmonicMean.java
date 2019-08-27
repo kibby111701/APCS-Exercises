3a719d74d0c9031b1602e61b79bed730491572a3
Chris Kraft <kibby111701@gmail.com>
2019-08-27 12:49:29 -0600

    Fixed and completed Harmonic mean code

--- Chapter13/HarmonicMean.java
+++ Chapter13/HarmonicMean.java
@@ -1,7 +1,24 @@
-public class HarmonicMean;
+import java.util.Scanner;
+
+public class HarmonicMean{
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        double x, y, arithMean, harmMean;
+
+        System.out.println("Enter X:");
+        x = scan.nextDouble();
+        System.out.println("Enter Y:");
+        y = scan.nextDouble();
+
+        arithMean = (x+y)/2;
+        harmMean = 2/((1/x) + (1/y));
+
+        System.out.println("Arithmetic mean: " + arithMean);
+        System.out.println("Harmonic mean: " + harmMean);
+
 
 
 
     }
+}
