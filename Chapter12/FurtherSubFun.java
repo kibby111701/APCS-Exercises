24926bf27a63924ba7817dea1be2dc46aad1ef68
Chris Kraft <kibby111701@gmail.com>
2019-08-27 12:22:46 -0600

    Finished exercise

--- Chapter12/FurtherSubFun.java
+++ Chapter12/FurtherSubFun.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class FurtherSubFun {
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        String sentence, sub;
+        int beginIndex, endIndex;
+
+        System.out.println("Enter a string");
+        sentence = scan.nextLine();
+        System.out.println("Enter beginning index");
+        beginIndex = scan.nextInt();
+        System.out.println("Enter ending index");
+        endIndex = scan.nextInt();
+
+        sub = sentence.substring(beginIndex, endIndex);
+        System.out.println("Original string: ");
+        System.out.println(sentence);
+        System.out.println("Substring: ");
+        System.out.println(sub);
+
+
     }
 }
