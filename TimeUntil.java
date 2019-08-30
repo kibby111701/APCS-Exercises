c8a032669bafe9ae0b312f6f354d1409bbae4cc6
Chris Kraft <kibby111701@gmail.com>
2019-08-30 12:54:55 -0600

    initial coding

---
+++ TimeUntil.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+
+public class TimeUntil{
+    public static void main(String[] args) {
+        int currentHour = LocalDateTime.now().getHour();
+        int currentMinute = LocalDateTime.now().getMinute();
+        int currentSecond = LocalDateTime.now().getSecond();
+
+
+    }
+
+
+}
