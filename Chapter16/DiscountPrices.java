232b4e75ee38389196caaf86880a17761983af94
Chris Kraft <kibby111701@gmail.com>
2019-09-06 12:34:33 -0600

    Finished discount code

--- Chapter16/DiscountPrices.java
+++ Chapter16/DiscountPrices.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class DiscountPrices{
     public static void main(String[] args) {
-        
+        int initialCost, discountCost;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the amount of the purchase in cents");
+        initialCost = scan.nextInt();
+
+        if (initialCost >= 1000){
+            discountCost = (initialCost/10) * 9;
+        }
+        else{
+            discountCost = initialCost;
+        }
+
+        System.out.println("The discount cost is: " + discountCost);
     }
-
-
-
 
 }
