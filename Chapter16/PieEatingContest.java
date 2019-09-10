c091289d51072d14e8f057ae4d6748780979f633
Chris Kraft <kibby111701@gmail.com>
2019-09-10 12:20:19 -0600

    Finished pie eating contest code

--- Chapter16/PieEatingContest.java
+++ Chapter16/PieEatingContest.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class PieEatingContest{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int contestantWeight;
+
+        System.out.println("How much do you weigh?");
+        contestantWeight = scan.nextInt();
+
+        if (contestantWeight - 250 < -30 || contestantWeight - 250 > 30){
+            System.out.println("You can't compete");
+        }
+        else{
+            System.out.println("You can compete");
+        }
+
+
     }
 
 
