8b4a171f9fa401c443cb9c03c40588c0992aa5e3
Chris Kraft <kibby111701@gmail.com>
2019-09-10 12:15:35 -0600

    Finished Last Chance Gas code

--- Chapter16/LastChanceGas.java
+++ Chapter16/LastChanceGas.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class LastChanceGas{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int tankCapacity, gageReading, milesPerGallon;
+
+        System.out.println("Tank capacity:");
+        tankCapacity = scan.nextInt();
+        System.out.println("Gage reading:");
+        gageReading = scan.nextInt();
+        System.out.println("Miles per Gallon");
+        milesPerGallon = scan.nextInt();
+
+        if (((tankCapacity * (gageReading/100)) * milesPerGallon) < 200){
+            System.out.println("Get gas!");
+        }
+        else{
+            System.out.println("Safe to proceed");
+        }
     }
 
 
