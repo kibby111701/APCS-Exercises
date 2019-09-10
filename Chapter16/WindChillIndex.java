3f8817719d96f9db535c9c39db3d28bfc52d24c5
Chris Kraft <kibby111701@gmail.com>
2019-09-10 12:33:54 -0600

    Finished WindChillIndex code

--- Chapter16/WindChillIndex.java
+++ Chapter16/WindChillIndex.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class WindChillIndex{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        double windSpeed, temperature, windChill;
+
+        System.out.println("Wind Speed:");
+        windSpeed = scan.nextDouble();
+        System.out.println("Temperature:");
+        temperature = scan.nextDouble();
+
+        if (windSpeed < 3 || temperature > 50){
+            windChill = temperature;
+        }
+        else{
+            windChill = 35.74 + (0.6215*temperature)- (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275*temperature*Math.pow(windSpeed, 0.16)); 
+        }
+
+        System.out.println("Wind chill: " + windChill);
 
 
     }
