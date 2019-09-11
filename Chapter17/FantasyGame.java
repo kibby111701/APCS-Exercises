54c91f334daa23f19cb618a8424cca1a19d3e15f
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2019-09-10 20:51:17 -0600

    Created fantasy game code

---
+++ Chapter17/FantasyGame.java
@@ -0,0 +1,11 @@
+public class FantasyGame{
+    public static void main(String[] args){
+        // int strength, health, luck;
+        // String str =
+
+        // System.out.println("Welcome to Crystal Sapphire Quest!")
+
+
+    }
+
+}
