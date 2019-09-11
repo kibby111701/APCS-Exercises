51bb382617804a544abf822d9386aeb6998f0cfd
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2019-09-10 21:09:39 -0600

    Finished Fantasy game code

--- Chapter17/FantasyGame.java
+++ Chapter17/FantasyGame.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class FantasyGame{
     public static void main(String[] args){
-        // int strength, health, luck;
-        // String str =
+        Scanner scan = new Scanner(System.in);
+        int strength, health, luck;
+        String name;
 
-        // System.out.println("Welcome to Crystal Sapphire Quest!")
+        System.out.println("Welcome to Crystal Sapphire Quest! Enter your name here:");
+        name = scan.nextLine();
+        System.out.println("Enter your strength");
+        strength = scan.nextInt();
+        System.out.println("Enter your health");
+        health = scan.nextInt();
+        System.out.println("Enter your luck");
+        luck = scan.nextInt();
 
+        if (strength + health + luck > 15){
+            System.out.println("Your character has is too powerful! Default values have been assigned");
+            strength = 5;
+            health = 5;
+            luck = 5;
+        }
+
+        System.out.println(name + " strength: " + strength + " , health: " + health + " , luck: " + luck);
 
     }
 
