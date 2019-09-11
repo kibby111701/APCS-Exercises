dc3d6c37c964e334814c5d8f3a544bdcc0bbf1d5
Chris Kraft <kibby111701@gmail.com>
2019-09-11 13:12:07 -0600

    Finished Income Tax code

---
+++ IncomeTax.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IncomeTax{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double yearlyIncome, taxesPaid, percentagePaid;
+
+        System.out.println("Enter your yearly income");
+        yearlyIncome = scan.nextDouble();
+
+        if (yearlyIncome < 15000){
+            taxesPaid = 0;
+        }
+        else if (yearlyIncome >= 15000 && yearlyIncome < 35000){
+            taxesPaid = 0.1 * (yearlyIncome - 15000);
+        }
+        else if (yearlyIncome >= 35000 && yearlyIncome < 65000){
+            taxesPaid = 2000 + (0.16 * (yearlyIncome - 35000));
+        }
+        else if (yearlyIncome >= 65000 && yearlyIncome < 105000){
+            taxesPaid = 6800 + (0.19 * (yearlyIncome - 65000));
+        }
+        else{
+            taxesPaid = 14400 + (0.22 * (yearlyIncome - 105000));
+        }
+
+        percentagePaid = (taxesPaid/yearlyIncome) * 100;
+
+        System.out.println("Total paid: $" + taxesPaid);
+        System.out.println(percentagePaid + "% of " + yearlyIncome);
+    }
+}
