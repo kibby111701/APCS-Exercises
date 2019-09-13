ed590397c5911606626d6dd0baaabeec4f3b9518
Chris Kraft <kibby111701@gmail.com>
2019-09-13 12:50:54 -0600

    Finished words by dots code

--- Chapter19/WordsByDots.java
+++ Chapter19/WordsByDots.java
@@ -2,7 +2,24 @@ import java.util.Scanner;
 
 public class WordsByDots{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String firstWord, secondWord, comboWord;
+        int comboLength;
+
+        System.out.println("Enter first word");
+        firstWord = scan.nextLine();
+        System.out.println("Enter second word");
+        secondWord = scan.nextLine();
+
+        comboWord = firstWord + secondWord;
+        comboLength = comboWord.length();
+
+        System.out.print(firstWord);
+        while (comboLength < 30){
+            System.out.print(".");
+            comboLength += 1;
+        }
+        System.out.print(secondWord);
     }
 
 }
