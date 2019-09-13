f5ed4c0eb6345b1f0e77384136e3b2ec5896db69
Chris Kraft <kibby111701@gmail.com>
2019-09-13 12:38:26 -0600

    Finished Sum of Integers code

--- Chapter19/SumOfIntegers.java
+++ Chapter19/SumOfIntegers.java
@@ -2,8 +2,26 @@ import java.util.Scanner;
 
 public class SumOfIntegers{
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int loopCurrentValue, endValue, loopSum, formulaSum;
+
+        System.out.println("Enter the ending value you want");
+        endValue = scan.nextInt();
+
+        loopSum = 0;
+        formulaSum = 0;
+        loopCurrentValue = 1;
         
+        while (loopCurrentValue <= endValue){
+            loopSum += loopCurrentValue;
+            loopCurrentValue += 1;
+        }
         
+        formulaSum = (endValue * (endValue + 1))/2;
+
+        System.out.println("Loop Sum = " + loopSum);
+        System.out.println("Formula Sum = " + formulaSum);
+
     }
 
 
