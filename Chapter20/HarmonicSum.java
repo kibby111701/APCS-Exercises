e68afb9342228fd469ffc6588a0e4cdbc07131d1
Chris Kraft <kibby111701@gmail.com>
2019-09-16 12:41:45 -0600

    Finished harmonic sum code

--- Chapter20/HarmonicSum.java
+++ Chapter20/HarmonicSum.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class HarmonicSum{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int finalNumber, counter;
+        double sum;
 
+        System.out.println("Enter the final number");
+        finalNumber = scan.nextInt();
+
+        sum = 0;
+        counter = 1;
+
+        while (counter <= finalNumber){
+            sum += 1.0/counter;
+            counter += 1;
+        }
+
+        System.out.println("The sum is " + sum);
     }
 
 }
