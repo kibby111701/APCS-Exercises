3af8f3ce242753116bd9dc92c986e0c5d63dc039
Chris Kraft <kibby111701@gmail.com>
2019-09-16 12:35:36 -0600

    Finished add up integers code

--- Chapter20/AddUpIntegers.java
+++ Chapter20/AddUpIntegers.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class AddUpIntegers{
     public static void main(String[] args) {
-        
-        
+        Scanner scan = new Scanner(System.in);
+        int intCounter, howManyInts, sum;
+
+        System.out.println("How many integers will be added?");
+        howManyInts = scan.nextInt();
+
+        intCounter = 0;
+        sum = 0;
+
+        while (intCounter < howManyInts){
+            System.out.println("Enter an integer:");
+            sum += scan.nextInt();
+            intCounter += 1;
+        }
+
+        System.out.println("The sum is " + sum);
+
     }
 
 }
