c126f60f7ad4d34bd369f142d6e3357c9e6c9200
Chris Kraft <kibby111701@gmail.com>
2019-09-18 12:41:56 -0600

    Declared variables

--- Chapter21/MultiplicationQuiz.java
+++ Chapter21/MultiplicationQuiz.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class MultiplicationQuiz{
     public static void main(String[] args) {
-        
+        // Scanner scan = new Scanner(System.in);
+        // int numberOne, numberTwo, userAnswer, questionCount, numberRight;
+
+        // questionCount = 1;
+        // numberRight = 0;
+
+        // while (questionCount<=10){
+
+
+
+        }
+
     }
 
 }
