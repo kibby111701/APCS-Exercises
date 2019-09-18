0b5f84bc7b97210268c7456226b5b404b04126c2
Chris Kraft <kibby111701@gmail.com>
2019-09-18 11:49:14 -0600

    finished power of number code

--- Chapter21/PowerOfNumber.java
+++ Chapter21/PowerOfNumber.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 
 public class PowerOfNumber{
     public static void main(String[] args) {
-        
-        
+        int count, powerRaised;
+        double whatNumber, sum;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter X");
+        whatNumber = scan.nextDouble();
+        System.out.println("Enter N");
+        powerRaised = scan.nextInt();
+
+        if (powerRaised <= 0){
+            System.out.println("N must be a positive integer");
+        }
+        else {
+            count = 0;
+            sum = 1;
+            while (count < powerRaised){
+                sum *= whatNumber;
+                count += 1;
+            }
+
+            System.out.println(whatNumber + " to the power of " + powerRaised + " is " + sum);
+        }
     }
 
 }
