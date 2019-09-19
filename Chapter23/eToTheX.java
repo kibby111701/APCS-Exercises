9d7fc5ef6c10bbfad89c458ef646f5f315a399ec
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2019-09-18 23:18:09 -0600

    Finished e to the x code

--- Chapter23/eToTheX.java
+++ Chapter23/eToTheX.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 
 public class eToTheX{
     public static void main(String[] args){
-        
+        Scanner scan = new Scanner(System.in);
+        double term, sum, powerRaised, n;
 
+        System.out.println("Enter X");
+        powerRaised = scan.nextInt();
 
+        term = 1;
+        sum = 1;
+        n = 1;
+
+        while (term >= 1e-12){
+            term *= (powerRaised/n);
+            sum += term;
+            n += 1;
+        }
+
+        System.out.println("With the series method: e^" + powerRaised + "=" + sum);
+        System.out.println("With exponents: e^" + powerRaised + "=" + Math.exp(powerRaised));
     }
 
 }
