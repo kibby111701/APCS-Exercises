d79fbe22e9ffad4819e62ad1e5728ef733e4b75f
Chris Kraft <kibby111701@gmail.com>
2019-09-23 12:52:02 -0600

    Finished Sheep herd size code

--- Chapter24/SheepHerdSize.java
+++ Chapter24/SheepHerdSize.java
@@ -1,9 +1,15 @@
-import java.util.Scanner;
-
 public class SheepHerdSize{
     public static void main(String[] args) {
-        
+        double n, power;
+        int t;
 
+        power = 0.83;
+        System.out.println("20");
+        for (t = 1; t < 26; t++ ){        
+           n = Math.round(220/(1+ (10 * power)));
+           power *= 0.83;
+           System.out.println(n); 
+        }
         
     }
 
