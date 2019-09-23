c08548fafc8d62fe503a3e8c4a154081960b4c6b
Chris Kraft <kibby111701@gmail.com>
2019-09-23 12:27:06 -0600

    finished sum of odd code

--- Chapter24/SumOfOdd.java
+++ Chapter24/SumOfOdd.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 public class SumOfOdd{
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n, startNumber, sum, nSquared;
         
+        System.out.println("Enter N:");
+        n = scan.nextInt();
+
+        sum = 0;
+        nSquared = n * n;
+        
+        for (startNumber = 1; startNumber <= n; startNumber = startNumber + 2){
+            sum += startNumber;
+        }
+        System.out.println(sum);
+        System.out.println(nSquared);
     }
 
 }
