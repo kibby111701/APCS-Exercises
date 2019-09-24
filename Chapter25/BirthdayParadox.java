4d9c8ab0ce5a64020c0ae24413305038bdb6a6a3
Chris Kraft <kibby111701@gmail.com>
2019-09-24 12:48:56 -0600

    finished birthday paradox code

--- Chapter25/BirthdayParadox.java
+++ Chapter25/BirthdayParadox.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class BirthdayParadox{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        double probability = 1;
+        int numberGuests;
 
-        
+        System.out.println("How many guests?");
+        numberGuests = scan.nextInt();
+
+        for (int count = 1; count <= numberGuests; count ++){
+            probability *= ((365-count)/365.0);
+
+        }
+
+        System.out.println(probability);
+
     }
 
 
