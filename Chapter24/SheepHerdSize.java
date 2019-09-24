0f8637042e5f92bb135dcc36a7c24efb35f4b27c
Chris Kraft <kibby111701@gmail.com>
2019-09-24 12:16:25 -0600

    revised sheep herd code to declare t in a different place

--- Chapter24/SheepHerdSize.java
+++ Chapter24/SheepHerdSize.java
@@ -1,11 +1,11 @@
 public class SheepHerdSize{
     public static void main(String[] args) {
         double n, power;
-        int t;
+
 
         power = 0.83;
         System.out.println("20");
-        for (t = 1; t < 26; t++ ){        
+        for (int t = 1; t < 26; t++ ){         
            n = Math.round(220/(1+ (10 * power)));
            power *= 0.83;
            System.out.println(n); 
