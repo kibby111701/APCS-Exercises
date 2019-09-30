88f6f21e9f0f8b8f1cc75f7e302432c3a213f221
Chris Kraft <kibby111701@gmail.com>
2019-09-30 12:54:55 -0600

    initialized password cracker

--- Chapter30/PasswordCracker.java
+++ Chapter30/PasswordCracker.java
@@ -2,9 +2,23 @@ import java.util.*;
 
 public class PasswordCracker{
     public static void main(String[] args) {
-        
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+        String password, possibility;
+        int char1, char2, char3, char4, char5, tryCount;
 
-        
+        System.out.println("Enter a password");
+        password = scan.nextLine();
+        possibility = "abcdefghijklmnopqrstuvwxyz ";
+
+        tryCount = 0;
+
+        while (!password.equals(char1 + char2 + char3 + char4 + char5){
+            char1 = possibility.charAt(rand.nextInt(27));
+            char2 = possibility.charAt(rand.nextInt(27));
+            char3 = possibility.charAt(rand.nextInt(27));
+        }
+
     }
 
 }
