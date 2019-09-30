9d7b544cfb2cf2909a2c090aca0d9e0768949f66
Chris Kraft <kibby111701@gmail.com>
2019-09-30 12:38:30 -0600

    finished guessing game

--- Chapter30/GuessingGame.java
+++ Chapter30/GuessingGame.java
@@ -2,7 +2,37 @@ import java.util.*;
 
 public class GuessingGame{
     public static void main(String[] args) {
+        int tryCount, guessNumber, answerNumber;
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+
+        answerNumber = rand.nextInt(10) + 1;
+        guessNumber = 0;
+        tryCount = 3;
         
+        System.out.println("I am thinking of a number between 1 and 10. Guess it in three tries.");
+        
+        while (tryCount > 0 && guessNumber != answerNumber){
+            guessNumber = scan.nextInt();
+
+            if (guessNumber == answerNumber){
+                System.out.println("Correct!");
+            }
+            else{
+                System.out.println("Wrong");
+                tryCount -= 1;
+            }
+
+        }
+
+        if (guessNumber == answerNumber){
+            System.out.println("You won!");
+        }
+
+        if (tryCount == 0){
+            System.out.println("You lost");
+            System.out.println("The correct number was " + answerNumber);
+        }
     }
 
 }
