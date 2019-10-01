9fdbf450d9212cf64ded33186e0b467e6ae3f62a
Chris Kraft <kibby111701@gmail.com>
2019-10-01 12:39:53 -0600

    finished password cracker

--- Chapter30/PasswordCracker.java
+++ Chapter30/PasswordCracker.java
@@ -4,21 +4,31 @@ public class PasswordCracker{
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
-        String password, possibility;
-        int char1, char2, char3, char4, char5, tryCount;
+        String password, possibility, guessPassword; 
+        int tryCount;
 
         System.out.println("Enter a password");
         password = scan.nextLine();
-        possibility = "abcdefghijklmnopqrstuvwxyz ";
+        possibility = "abcdefghijklmnopqrstuvwxyz";
 
         tryCount = 0;
+        int letterCounter = 0;
+        guessPassword = "";
 
-        while (!password.equals(char1 + char2 + char3 + char4 + char5){
-            char1 = possibility.charAt(rand.nextInt(27));
-            char2 = possibility.charAt(rand.nextInt(27));
-            char3 = possibility.charAt(rand.nextInt(27));
+        while (!password.equals(guessPassword)){
+            guessPassword = "";
+            letterCounter = 0;
+            while (letterCounter < password.length()){
+                guessPassword = guessPassword + possibility.charAt(rand.nextInt(possibility.length()));
+                letterCounter +=1;
+            }
+            
+            System.out.println(tryCount);
+            tryCount += 1;
         }
 
+        System.out.println("The password is " +guessPassword);
+        System.out.println("It took " + tryCount + " tries");
     }
 
 }
