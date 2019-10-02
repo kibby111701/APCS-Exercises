31ed44b8c09dde6ca8e6db27c1f177ea7475fb58
Stephanie Kraft <stephaniekraft@Stephanies-Mac-mini.local>
2019-10-01 20:13:25 -0600

    finished 2d random walk code

--- Chapter30/TwoDRandomWalk.java
+++ Chapter30/TwoDRandomWalk.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 public class TwoDRandomWalk{
     public static void main(String[] args) {
+        Random rand = new Random();
         Scanner scan = new Scanner(System.in);
         double x, y, xPlus, yPlus, distance; 
-        int i, finalStep;
+        int finalStep;
         x = 0;
         y = 0;
 
         System.out.println("Enter the number of iterations you want");
         finalStep = scan.nextInt();
-        i = 0
         
-        while (i < finalStep){
-            xPlus = (Math.random()*2) - 1;
-            yPlus = (Math.random()*2) - 1;
+        for (int i = 0; i < finalStep; i++){
+            xPlus = rand.nextDouble()*2 - 1;
+            yPlus = rand.nextDouble()*2 - 1;
             x += xPlus;
             y += yPlus;
         }
+
+        distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("Distance: " + distance);
     }
 
 }
