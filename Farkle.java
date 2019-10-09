c46580116316925afe5609ee20f843fe4a2946a5
Chris Kraft <kibby111701@gmail.com>
2019-10-09 13:13:33 -0600

    finished player 1. It's not a bug, its a feature.

--- Farkle.java
+++ Farkle.java
@@ -1,7 +1,4 @@
-
 import java.util.*;
-
-
 
 public class Farkle{
     public static void main(String[] args) {
@@ -9,10 +6,11 @@ public class Farkle{
         Random rand = new Random();
         int scoreOne, scoreTwo, workingSumOne, workingSumTwo, dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix, removeTimes;
         String continueOne, continueTwo, keepOne, keepTwo;
-        boolean farkle, threeOfAKind;
+        boolean farkle;
 
         scoreOne = 0;
         scoreTwo = 0;
+
 
         while (scoreOne < 10000 && scoreTwo < 10000){
             continueOne = "";
@@ -24,6 +22,7 @@ public class Farkle{
             dieCount = 6;
             removeTimes = 0;
             farkle = false;
+
 
             while (!continueOne.equals("n") && farkle == false && dieCount > 0){
                 System.out.println("Player One, press 'y' to roll your remaining dice. Press 'n' to back out.");
@@ -44,6 +43,12 @@ public class Farkle{
                         System.out.println("Die 4:" + dieFour);
                         System.out.println("Die 5:" + dieFive);
                         System.out.println("Die 6:" + dieSix);
+
+                        if (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) != -1){
+                            System.out.println("You have a three of a kind!");
+                            workingSumOne += (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix));
+                            removeTimes += 3;
+                        }
 
                         if (dieOne == 1 || dieOne == 5){
                             System.out.println("Keep Die 1? y/n");
@@ -150,7 +155,7 @@ public class Farkle{
 
                         
 
-                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5) && (dieFive != 1 && dieFive != 5) && (dieSix != 1 && dieSix != 5)) {
+                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5) && (dieFive != 1 && dieFive != 5) && (dieSix != 1 && dieSix != 5) && threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) == -1) {
                             System.out.println("Farkle!");
                             workingSumOne = 0;
                             farkle = true;
@@ -173,12 +178,19 @@ public class Farkle{
                         dieThree = rand.nextInt(6) + 1;
                         dieFour = rand.nextInt(6) + 1;
                         dieFive = rand.nextInt(6) + 1;
+                        dieSix = 0;
 
                         System.out.println("Die 1:" + dieOne);
                         System.out.println("Die 2:" + dieTwo);
                         System.out.println("Die 3:" + dieThree);
                         System.out.println("Die 4:" + dieFour);
                         System.out.println("Die 5:" + dieFive);
+
+                        if (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) != -1){
+                            System.out.println("You have a three of a kind!");
+                            workingSumOne += (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix));
+                            removeTimes += 3;
+                        }
 
                         if (dieOne == 1 || dieOne == 5){
                             System.out.println("Keep Die 1? y/n");
@@ -268,7 +280,7 @@ public class Farkle{
                             }
                         }
 
-                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5) && (dieFive != 1 && dieFive != 5)) {
+                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5) && (dieFive != 1 && dieFive != 5) && threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) == -1) {
                             System.out.println("Farkle!");
                             workingSumOne = 0;
                             farkle = true;
@@ -293,11 +305,19 @@ public class Farkle{
                         dieTwo = rand.nextInt(6) + 1;
                         dieThree = rand.nextInt(6) + 1;
                         dieFour = rand.nextInt(6) + 1;
+                        dieFive = 0;
+                        dieSix = 0;
 
                         System.out.println("Die 1:" + dieOne);
                         System.out.println("Die 2:" + dieTwo);
                         System.out.println("Die 3:" + dieThree);
                         System.out.println("Die 4:" + dieFour);
+
+                        if (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) != -1){
+                            System.out.println("You have a three of a kind!");
+                            workingSumOne += (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix));
+                            removeTimes += 3;
+                        }
 
                         if (dieOne == 1 || dieOne == 5){
                             System.out.println("Keep Die 1? y/n");
@@ -369,7 +389,7 @@ public class Farkle{
                             }
                         }
 
-                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5)){
+                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && (dieFour != 1 && dieFour != 5) && threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) == -1){
                             System.out.println("Farkle!");
                             workingSumOne = 0;
                             farkle = true;
@@ -391,10 +411,19 @@ public class Farkle{
                         dieOne = rand.nextInt(6) + 1;
                         dieTwo = rand.nextInt(6) + 1;
                         dieThree = rand.nextInt(6) + 1;
+                        dieFour = 0;
+                        dieFive = 0;
+                        dieSix = 0;
 
                         System.out.println("Die 1:" + dieOne);
                         System.out.println("Die 2:" + dieTwo);
                         System.out.println("Die 3:" + dieThree);
+
+                        if (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) != -1){
+                            System.out.println("You have a three of a kind!");
+                            workingSumOne += (threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix));
+                            removeTimes += 3;
+                        }
 
                         if (dieOne == 1 || dieOne == 5){
                             System.out.println("Keep Die 1? y/n");
@@ -444,7 +473,7 @@ public class Farkle{
                             }
                         }
 
-                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5)){
+                        if ((dieOne != 1 && dieOne != 5) && (dieTwo != 1 && dieTwo != 5) && (dieThree != 1 && dieThree != 5) && threeOfKind(dieCount, dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix) == -1){
                             System.out.println("Farkle!");
                             workingSumOne = 0;
                             farkle = true;
@@ -568,5 +597,55 @@ public class Farkle{
 
     }
 
+    public static int threeOfKind(int dieCount, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive, int dieSix){
+        int numEqual = 0;
+
+        for (int i = 1; i < dieCount; i++){
+            if (dieOne == i){
+                numEqual ++;
+            }
+            if (dieTwo == i){
+                numEqual ++;
+            }
+            if (dieThree == i){
+                numEqual ++;
+            }
+            if (dieFour == i){
+                numEqual ++;
+            }
+            if (dieFive == i){
+                numEqual ++;
+            }
+            if (dieSix == i){
+                numEqual ++;
+            }
+
+            if (numEqual >= 3){
+                if (i == 1){
+                    return 1000;
+                }
+                else if (i == 2){
+                    return 200;
+                }
+                else if (i == 3){
+                    return 300;
+                }
+                else if (i == 4){
+                    return 400;
+                }
+                else if (i == 5){
+                    return 500;
+                }
+                else{
+                    return 600;
+                }
+            }
+
+            numEqual = 0;
+
+        }
+
+        return -1;
+    }
 
 }
