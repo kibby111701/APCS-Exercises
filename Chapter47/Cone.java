5ab656ebe0fbd52835db01dae3c8dc5336af4e8c
Chris Kraft <kibby111701@gmail.com>
2019-10-25 12:45:38 -0600

    created Cone files

---
+++ Chapter47/Cone.java
@@ -0,0 +1,29 @@
+public class Cone{
+    
+    private double radius;
+    private double height;
+
+    public Cone(double radius, double height){
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+
+
+}
---
+++ Chapter47/ConeTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConeTest{
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        double radius, height;
+
+        System.out.println("Enter the radius");
+        radius = scan.nextDouble();
+
+        System.out.println("Enter the height");
+        height = scan.nextDouble();
+
+        
+    }
+
+}
