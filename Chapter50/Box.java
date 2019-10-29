3e4d3d016347bb3e86927619ee7d65a1ffbcc8ae
Chris Kraft <kibby111701@gmail.com>
2019-10-29 12:51:21 -0600

    finished with oldbox, still need bigger and nesting boxes

--- Chapter50/Box.java
+++ Chapter50/Box.java
@@ -1,5 +1,58 @@
 public class Box{
-
-
     
+    public double length;
+    public double width;
+    public double height;
+
+    public Box(double width, double height, double length){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Box(double side){
+        this.length = side;
+        this.width = side;
+        this.height = side;
+    }
+
+    public Box(Box oldbox){
+        this.length = oldbox.getLength();
+        this.width = oldbox.getWidth();
+        this.height = oldbox.getHeight();
+    }
+
+    public double getLength(){
+        return this.length;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public double getWidth(){
+        return this.width;
+    }
+
+    public double volume(){
+        return length * width * height;
+    }
+
+    private double topArea(){
+        return length * width;
+    }
+
+    private double sideArea(){
+        return width * height;
+    }
+
+    private double faceArea(){
+        return length * height;
+    }
+
+    public double area(){
+        return 2*(faceArea() + sideArea() + topArea());
+    }
+
+
 }
--- Chapter50/BoxTester.java
+++ Chapter50/BoxTester.java
@@ -1,6 +1,14 @@
 public class BoxTester{
     public static void main(String[] args) {
         
+        Box box = new Box(2.5, 5.0, 6.0);
+
+        Box oldBox = new Box(box);
+
+        System.out.println("Area: " + box.area() + " Volume: " + box.volume());
+
+        System.out.println("Area: " + oldBox.area() + " Volume: " + oldBox.volume());
+
     }
 
 
