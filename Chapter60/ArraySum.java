5d7b638c4c4152be101292139f47908df4557e2e
Chris Kraft <kibby111701@gmail.com>
2019-11-19 12:35:33 -0700

    created and finished array sum

---
+++ Chapter60/ArraySum.java
@@ -0,0 +1,11 @@
+public class ArraySum{
+    public static void main(String[] args) {
+        int[] val = {0,1,2,3};
+
+        int sum = val[0] + val[1] + val[2] + val[3];
+
+        System.out.println("Sum = " + sum);
+    }
+
+
+}
